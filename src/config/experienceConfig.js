5f901243f8441d82b858b6c0e7a01da79cdca20e
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-01-18 11:20:27 +0530

    Experience updated

--- src/config/experienceConfig.js
+++ src/config/experienceConfig.js
@@ -1,12 +1,27 @@
 export const experiences = [
   {
     id: 1,
+    company: 'Valor PayTech · Full-time',
+    logoAssetName: 'valor',
+    designation: 'Software Engineer',
+    tenure: 'December 2024 - Present',
+    location: 'Chennai, India - Onsite',
+    isCurrent: true,
+    description: [
+      'Implement feature enhancements based on user feedback, contributing to continuous improvement and increased customer satisfaction',
+      'Identify and Resolve software bugs through thorough testing and debugging, ensuring a seamless user experience and maintaining application stability',
+      'Collaborated with multiple internal teams to gather requirements and prioritize bug fixes and feature requests, ensuring alignment with overall project objectives',
+      'Conduct peer reviews fostering a culture of collaboration and continuous learning within the team.'
+    ]
+  },
+  {
+    id: 2,
     company: 'Zealeye.AI Tech Solution · Full-time',
     logoAssetName: 'zealeye',
     designation: 'Software Developer',
-    tenure: 'March 2023 - Present',
+    tenure: 'March 2023 - November 2024',
     location: 'Chennai, India - Onsite',
-    isCurrent: true,
+    isCurrent: false,
     description: [
       'Led a team in backend development, managing the complete lifecycle of backend processes and APIs',
       'Enhanced system performance and reliability by implementing best practices and utilizing AWS cloud services.',
@@ -15,7 +30,7 @@ export const experiences = [
     ]
   },
   {
-    id: 2,
+    id: 3,
     company: 'Cognizant Technology Solutions · Full-time',
     logoAssetName: 'cognizant',
     designation: 'Systems Engineer',
@@ -30,7 +45,7 @@ export const experiences = [
     ]
   },
   {
-    id: 3,
+    id: 4,
     company: 'ByteBlanket · Full-time',
     logoAssetName: 'byteblanket',
     designation: 'Junior QA',
@@ -44,7 +59,7 @@ export const experiences = [
     ]
   },
   {
-    id: 4,
+    id: 5,
     company: 'ByteBlanket · Internship',
     logoAssetName: 'byteblanket',
     designation: 'Software Developer',
