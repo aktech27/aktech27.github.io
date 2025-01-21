3cf25f0ae371df57d9e20ef4bb8cd18beb435d28
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-01-21 21:27:49 +0530

    Minor Changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -13,12 +13,12 @@ const AboutSection = () => {
         <div className='h-[30vh] lg:h-auto lg:w-[40%]'>
           <PcModelCanvas />
         </div>
-        <div className='lg:w-[60%] md:text-[1.35rem]'>
+        <div className='lg:w-[60%] md:text-[1.35rem] text-justify'>
           <div>
             I specialize in <TextHighlight>Node.js</TextHighlight>, utilizing <TextHighlight>Express</TextHighlight> for backend APIs and <TextHighlight>React</TextHighlight> for frontend UI.<TextHighlight>PostgreSQL</TextHighlight> and <TextHighlight>MongoDB</TextHighlight> are my go-to SQL and NoSQL databases, but I&apos;m open to exploring others as well. I’m always keen on <TextHighlight>learning new technologies</TextHighlight> and falling in love with the <TextHighlight>process of mastering</TextHighlight> them.
           </div>
           <div>
-            When I&apos;m not coding, I love surfing YouTube, always looking for content on gaming, PC building, and system customization. I also enjoy solving Rubik&apos;s cubes and sometimes tackling Sudoku.
+            When I&apos;m not coding, I love <TextHighlight>Surfing YouTube</TextHighlight>, always looking for content on gaming, PC building, and system customization. I also enjoy solving <TextHighlight> Rubik&apos;s cubes</TextHighlight> and sometimes tackling <TextHighlight>Sudoku</TextHighlight>.
           </div>
         </div>
       </div>
