e92d8f957a1729f9e3bc341c03ad406cb1efa569
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-01-31 21:33:25 +0530

    [WIP] Animate Summary numbers

--- src/components/HeaderSummary.jsx
+++ src/components/HeaderSummary.jsx
@@ -1,9 +1,17 @@
 'use client';
 
 import { headerSummaryStats } from '@/config/headerConfig';
-import { motion } from 'motion/react';
+import { motion, useMotionValue, animate, useTransform } from 'motion/react';
+import { useEffect } from 'react';
 
 const HeaderSummary = () => {
+  const count = useMotionValue(0);
+  const number = useTransform(() => Math.round(count.get()));
+  useEffect(() => {
+    const controls = animate(count, 100, { duration: 3 });
+    return () => controls.stop();
+  }, [count]);
+
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0 }}
@@ -18,7 +26,7 @@ const HeaderSummary = () => {
         {
           headerSummaryStats.map((item, index) => (
             <div key={index} className="flex-1 flex gap-4 items-center justify-center">
-              <div className='text-3xl lg:text-5xl font-extrabold'>{item.number}</div>
+              <div className='text-3xl lg:text-5xl font-extrabold'><motion.span>{number}</motion.span>+</div>
               <div className='text-sm'>{item.text}</div>
             </div>
           ))
