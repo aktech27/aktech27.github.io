ef5a351a5300e464e0228a826e6130940b8e256a
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2026-03-28 12:41:50 +0530

    Update Resume link

--- src/config/linksConfig.js
+++ src/config/linksConfig.js
@@ -1,5 +1,5 @@
 export const links = {
-  resume: "https://drive.google.com/uc?export=download&id=1Y_LxopCKEVhqEHZVIp7fswC5L4_jn96o",
+  resume: "https://drive.google.com/uc?export=download&id=1fbYasujmiErkOaGao3W3VPVMfpAWje-m",
   linkedin: "https://www.linkedin.com/in/ahamed-kabeer-aktech27/",
   github: "https://github.com/aktech27",
   leetcode: "https://leetcode.com/u/aktech27/"
